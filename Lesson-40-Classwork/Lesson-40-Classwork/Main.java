5640f07636f0d2539e182cb626589a2aa558e4b2
AshtonSusankar887 <ashtons27@nycstudents.net>
2026-03-12 00:30:29 +0000

    3/11/2026

---
+++ Lesson-40-Classwork/Lesson-40-Classwork/Main.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.sql.*;
+//For compiling on the shell on repl: Same on mac
+//javac -cp sqlite-jdbc-3.23.1.jar: Main.java
+
+//Use for windows
+//javac -cp sqlite-jdbc-3.23.1.jar; ServerExample.java
+class Main {
+
+ public static void main(String[] args)throws IOException{
+    (new Main()).init();
+  }
+
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init() {
+     
+
+      String queryResult="";
+      String sql = "";
+
+		  Database   db =  new  Database("jdbc:sqlite:cr101.db" );	
+
+      sql = " Select * From cr101 where "  ;
+      sql+= " period = 3 and room = 106";
+
+      queryResult = db.runSQL(sql,"table-auto");
+
+      print(queryResult);	
+
+      }    
+}
